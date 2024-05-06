1dd270b74ed8cb9b7534a2911eb2c902fbac82ce
shaikh aamir <101973622+shaikhaamir2508@users.noreply.github.com>
2024-05-06 15:37:21 +0530

    mongo url

--- DB/index.js
+++ DB/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost:27017/MERN-TODO")
+  .connect("mongodb+srv://shaikhaamir2508:1lIJgUwp9kAjdVSy@cluster0.tajozc7.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log("db connected");
   })
